16b0239ec4e70d8ce4b8fc09d5c3665bc280ba54
loneyb11 <59574123+loneyb11@users.noreply.github.com>
2020-02-22 23:11:25 -0600

    creating outlines for script

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-
+// variables
+// functions 
+// click events for input caledar events
+// click events to clear
+// save userInput
+// storeEvents
+// create divs for each hour
+// clear local storage
