e10c3383fcc9e19f0ac0ca1e6284695fb3f57205
loneyb11 <59574123+loneyb11@users.noreply.github.com>
2020-02-24 23:54:15 -0600

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// variables
-// functions 
-// click events for input caledar events
-// click events to clear
-// save userInput
-// storeEvents
-// create divs for each hour
-// clear local storage
